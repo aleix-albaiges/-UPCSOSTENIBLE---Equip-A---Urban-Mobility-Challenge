a2af5081fdce53e48cb9431bf68b38e7f3a86835
Adriacz <82530022+Adriacz@users.noreply.github.com>
2023-11-11 20:47:27 +0100

    Add files via upload

--- grafic_barres_queso.py
+++ grafic_barres_queso.py
@@ -103,6 +103,34 @@ def graph(students: dict[int, Student]) -> None:
             others[1] += 1
 
 
+# How do people exactly move from their home to their school? 
+    vals = [pt[0], am[0], private_pol[0], private_no_pol[0], pt_am[0],pt_priv_pol[0], pt_priv_no_pol[0], am_priv_pol[0], am_priv_no_pol[0],
+        pt_am_priv_pol[0], pt_am_priv_no_pol[0], others[0]]
+    
+    fig, ax = plt.subplots(figsize = (16,9))
+    ax.plot("PT", label = "PT = Public transport", color = '#1f77b4')
+    ax.plot("AM", label = "AM = Active mobility", color = '#ff7f0e')
+    ax.plot("Priv_p", label = "Priv_p = Private pollution", color = '#2ca02c')
+    ax.plot("Priv_no_p", label = "Priv_no_p = Private no pollution", color = '#d62728')
+    ax.plot("PT+AM", label = "Public transpot + Active mobility", color = '#9467bd')
+    ax.plot("PT+Priv_p", label = "Public transport + Private pollution", color = '#8c564b')
+    ax.plot("PT+Priv_no_p", label = "Public transport + Private no pollution", color = '#e377c2')
+    ax.plot("Pirv_p+AM", label = "Private pollution + active mobility", color = '#7f7f7f')
+    ax.plot("Priv_no_p+AM", label = "Private no pollution + Active mobility", color = '#bcbd22') 
+    ax.plot("PT+AM+Priv_p", label = "Pubic transport + Activity mobility, Private pollution", color = '#17becf') 
+    ax.plot("PT+AM+P_no_p", label = "Public transport, Active mobility, Public no pollution", color = '#ff5733')
+    ax.plot("Other ", label = "Other",color = '#33ff57')
+
+    leg = ax.legend(loc='lower left', shadow=True, fontsize='x-small', title = 'How do people exactly move from their home to their school?')
+    #colors = ['blue','red','orange','green','blue','purple', 'pink', 'yellow', 'black', 'grey','brown', 'beige', 'light blue']
+    colors = colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd','#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf','#ff5733', '#33ff57']
+    ax.pie(vals, colors = colors)
+    plt.show() 
+    
+
+
+
+
     groups = ["PT", "AM", "Priv_p", "Priv_no_p", "PT+AM", "PT+Priv_p","PT+Priv_no_p", "Pirv_p+AM", "Priv_no_p+AM", "PT+AM+Priv_p", "PT+AM+P_no_p", "Others"]
     valors = [pt[0] + pt[1], am[0] + am[1], private_pol[0] + private_pol[1], private_no_pol[0] +private_no_pol[1], pt_am[0] + pt_am[1],
         pt_priv_pol[0] + pt_priv_pol[1], pt_priv_no_pol[0] + pt_priv_no_pol[1], am_priv_pol[0] + am_priv_pol[1], am_priv_no_pol[0] + am_priv_no_pol[1],
@@ -128,7 +156,7 @@ def graph(students: dict[int, Student]) -> None:
     ax.plot("Other ", label = "Other",color = '#33ff57')
 
 
-    legend = ax.legend(loc='upper left', shadow=True, fontsize='small')
+    legend = ax.legend(loc='upper left', shadow=True, fontsize='small', title = "General graphic")
     #colors = ['blue','red','orange','green','blue','purple', 'pink', 'yellow', 'black', 'grey','brown', 'beige', 'light blue']
     colors = colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd','#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf','#ff5733', '#33ff57']
     ax.pie(valors, colors = colors)
